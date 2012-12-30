5f1064dfa66ff8b172ce5ff38e2565b81dd71772
ledenis <ledenis.git@gmail.com>
2012-12-30 19:14:30 +0100

    Added a button to switch between Html Mode and Text Mode

--- Editor/script.js
+++ Editor/script.js
@@ -1,4 +1,23 @@
+var isHtmlMode = false;
 
 document.onreadystatechange = function() {
 	frameContent.document.designMode = "On";
+	
+	document.getElementById('switchMode').onclick = function() {
+		isHtmlMode = !isHtmlMode;
+		setHtmlMode(isHtmlMode);
+	};
 };
+
+function setHtmlMode(mode) {
+	var content = '';
+	if (mode) { // HTML mode
+		// show the HTML text
+		content = frameContent.document.body.innerHTML;
+		frameContent.document.body.innerText = content;
+	}
+	else { // Text mode
+		content = frameContent.document.body.innerText;
+		frameContent.document.body.innerHTML = content;
+	}
+}
